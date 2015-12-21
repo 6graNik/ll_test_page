5b4ecd61e211c6088ccce8777d8d7b7c20cd31ed
Tigran Kaginyan <6granik@gmail.com>
2015-12-22 00:11:24 +0300

    Накинул скриптов на выбор языка.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
-/**
- * Created by Тигран on 09.12.2015.
- */
+var ddButtons = $('.current_lang_title_span'),
+    ddBoxButton = $('.current_lang_title'),
+    dirChanger = $('.dir_changer'),
+    ddUl = $('.current_lang_title__dd_menu'),
+    ddLi = $('.current_lang_title__dd_menu li');
+
+
+$(window).on('click', function (e) {
+
+    var target = $(e.target);
+    if (!target.closest(ddBoxButton).length)
+        $(ddUl).slideUp();
+});
+
+ddButtons.on('click', function () {
+    var currentUl = $(this).parent().find(ddUl);
+    $(ddUl).slideUp();
+    currentUl.stop().slideToggle();
+});
+
+ddLi.on('click', function () {
+    var newTitle = $(this).find('.dd_menu__lng_title').text();
+    $(this).parent().parent().find('span').first().text(newTitle);
+});
+
+
+dirChanger.on('click', function () {
+    var textOfBtnFirst = $(ddButtons[0]).text(),
+        textOfBtnSec = $(ddButtons[1]).text();
+
+    $(ddButtons[0]).text(textOfBtnSec);
+    $(ddButtons[1]).text(textOfBtnFirst);
+
+
+});
